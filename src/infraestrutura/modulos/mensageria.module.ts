8b44604b8a2b75c938bb1421016c8d55f69c8621
guilhermeo97 <oliveiraguilherme591@gmail.com>
2025-03-26 13:06:11 -0300

    fix: ajustes de ambiente

--- src/infraestrutura/modulos/mensageria.module.ts
+++ src/infraestrutura/modulos/mensageria.module.ts
@@ -2,37 +2,46 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { PublicarEventoPagamento } from '../eventos/publicadores/publicar-evento-pagamento.publisher';
 import { EmitirEventosPagamento } from 'src/aplicacao/casos de uso/emitir-pagamento.use-case';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
+        imports: [ConfigModule],
         name: 'FilaPlanosAtivos',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            'amqps://wzdzpmja:yibUYRiKSYiLdR_yBFE0PQSPjBUGSdhd@jaragua.lmq.cloudamqp.com/wzdzpmja',
-          ],
-          exchange: 'pagamento_ex',
-          routingKey: 'pagamento.novo',
-          queue: 'sistema_planos_ativos_novo_pagamento',
-          queueOptions: { durable: true },
-        },
+        useFactory: async (configService: ConfigService) => ({
+          name: 'FilaPlanosAtivos',
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RMQ_URL') as string],
+            exchange: configService.get<string>('RMQ_EXCHANGE') as string,
+            routingKey: configService.get<string>('RMQ_ROUTING_KEY') as string,
+            queue: configService.get<string>(
+              'RMQ_FILA_SERVICO_PLANOS_ATIVOS',
+            ) as string,
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
+        imports: [ConfigModule],
         name: 'FilaSistemaGestao',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            'amqps://wzdzpmja:yibUYRiKSYiLdR_yBFE0PQSPjBUGSdhd@jaragua.lmq.cloudamqp.com/wzdzpmja',
-          ],
-          exchange: 'pagamento_ex',
-          routingKey: 'pagamento.novo',
-          queue: 'sistema_gestao_novo_pagamento',
-          queueOptions: { durable: true },
-        },
+        useFactory: async (configService: ConfigService) => ({
+          name: 'FilaSistemaGestao',
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RMQ_URL') as string],
+            exchange: configService.get<string>('RMQ_EXCHANGE') as string,
+            routingKey: configService.get<string>('RMQ_ROUTING_KEY') as string,
+            queue: configService.get<string>(
+              'RMQ_FILA_SERVICO_GESTAO',
+            ) as string,
+          },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
@@ -40,48 +49,3 @@ import { EmitirEventosPagamento } from 'src/aplicacao/casos de uso/emitir-pagame
   exports: [ClientsModule, PublicarEventoPagamento, EmitirEventosPagamento],
 })
 export class MensageriaModule {}
-
-// import { Module } from '@nestjs/common';
-// import { ClientsModule, Transport } from '@nestjs/microservices';
-// import { EmitirEventosPagamento } from 'src/aplicacao/casos de uso/emitir-pagamento.use-case';
-// import { PublicarEventoPagamento } from '../eventos/publicadores/publicar-evento-pagamento.publisher';
-
-// @Module({
-//   imports: [
-//     ClientsModule.register([
-//       {
-//         name: 'GestaoOperadoraTelecom',
-//         transport: Transport.RMQ,
-//         options: {
-//           urls: [
-//             'amqps://wzdzpmja:yibUYRiKSYiLdR_yBFE0PQSPjBUGSdhd@jaragua.lmq.cloudamqp.com/wzdzpmja',
-//           ],
-//           queueOptions: { durable: true },
-//         },
-//       },
-//     ]),
-//   ],
-//   providers: [EmitirEventosPagamento, PublicarEventoPagamento],
-//   exports: [ClientsModule, EmitirEventosPagamento, PublicarEventoPagamento],
-// })
-// export class MensageriaModule {}
-
-// providers: [
-//   {
-//     provide: 'GestaoOperadoraTelecom',
-//     useFactory: () => {
-//       return ClientProxyFactory.create({
-//         transport: Transport.RMQ,
-//         options: {
-//           urls: [
-//             'amqps://wzdzpmja:yibUYRiKSYiLdR_yBFE0PQSPjBUGSdhd@jaragua.lmq.cloudamqp.com/wzdzpmja',
-//           ], // CloudAMQP
-//           exchange: 'ex_pagamento',
-//           // routingKey: 'pagamento.novo',
-//           // exchangeType: 'fanout',
-//           queue: 'ignorar_fila_produtor', // evita a criação da `default`
-//           queueOptions: { durable: false },
-//         },
-//       });
-//     },
-//   },
